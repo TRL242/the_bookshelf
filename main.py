13c28148f9cbc7b96eac92af1658a7b79ae3b13d
trl242 <royaurallockhart@gmail.com>
2021-03-25 00:00:36 -0700

    adding database after adding books

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
 
 db = sqlite3.connect("books-collection.db")
+cursor = db.cursor()
+cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
 
 app = Flask(__name__)
 
